e95b4c10a31b75931d4d2ca8345e867df880b52c
sunshine772 <nestor2397@gmail.com>
2024-05-29 21:50:05 -0400

    feat:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,7 @@ import {
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppLayoutModule } from './layout/app.layout.module';
-import { ProductService } from './demo/service/product.service';
-import { CountryService } from './demo/service/country.service';
-import { CustomerService } from './demo/service/customer.service';
-import { EventService } from './demo/service/event.service';
-import { IconService } from './demo/service/icon.service';
-import { NodeService } from './demo/service/node.service';
-import { PhotoService } from './demo/service/photo.service';
+
 import { JWT_OPTIONS, JwtHelperService } from '@auth0/angular-jwt';
 import { AuthInterceptor } from './api/interceptors/auth/auth.interceptor';
 import { NotfoundComponent } from './api/components/notfound/notfound.component';
@@ -24,13 +18,6 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
     imports: [AppRoutingModule, AppLayoutModule],
     providers: [
         { provide: LocationStrategy, useClass: PathLocationStrategy },
-        CountryService,
-        CustomerService,
-        EventService,
-        IconService,
-        NodeService,
-        PhotoService,
-        ProductService,
         AuthInterceptor,
         JwtHelperService,
         { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
